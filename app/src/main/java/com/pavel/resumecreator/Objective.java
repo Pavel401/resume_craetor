847c5cb701c4509c23f2bcb436b69bdaf0ccafc9
Pavel401 <pavelalam401@gmail.com>
2021-01-13 20:59:41 +0530

    Commit Message -
    1.) All Details pages are added in resource file and onclick card listner added

---
+++ app/src/main/java/com/pavel/resumecreator/Objective.java
@@ -0,0 +1,14 @@
+package com.pavel.resumecreator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Objective extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_objective);
+    }
+}
--- app/src/main/java/com/pavel/resumecreator/details_page.java
+++ app/src/main/java/com/pavel/resumecreator/details_page.java
@@ -2,11 +2,16 @@ package com.pavel.resumecreator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
-public class details_page extends AppCompatActivity {
+public class details_page extends AppCompatActivity implements View.OnClickListener {
+    public CardView card1, card2, card3, card4, card5, card6, card7, card8;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,66 @@ public class details_page extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
+
+        card1 = (CardView) findViewById(R.id.c1);
+        card2 = (CardView) findViewById(R.id.c2);
+        card3 = (CardView) findViewById(R.id.c3);
+        card4 = (CardView) findViewById(R.id.c4);
+        card5 = (CardView) findViewById(R.id.c5);
+        card6 = (CardView) findViewById(R.id.c6);
+        card7 = (CardView) findViewById(R.id.c7);
+        card8 = (CardView) findViewById(R.id.c8);
+
+        card1.setOnClickListener(this);
+        card2.setOnClickListener(this);
+        card3.setOnClickListener(this);
+        card4.setOnClickListener(this);
+        card5.setOnClickListener(this);
+        card6.setOnClickListener(this);
+        card7.setOnClickListener(this);
+        card8.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()){
+            case R.id.c1:
+                i= new Intent(details_page.this,personal_details.class);
+                startActivity(i);
+                break;
+            case R.id.c2:
+                i= new Intent(details_page.this,Education_details.class);
+                startActivity(i);
+                break;
+            case R.id.c3:
+                i=new Intent(details_page.this,Experience.class);
+                startActivity(i);
+                break;
+            case R.id.c4:
+                i=new Intent(details_page.this,Skills.class);
+                startActivity(i);
+                break;
+            case R.id.c5:
+                i=new Intent(details_page.this,Objective.class);
+                startActivity(i);
+                break;
+            case R.id.c6:
+                i=new Intent(details_page.this,References.class);
+                startActivity(i);
+                break;
+            case R.id.c7:
+                i=new Intent(details_page.this,Projects.class);
+                startActivity(i);
+                break;
+            case R.id.c8:
+                i=new Intent(details_page.this,Achievements.class);
+                startActivity(i);
+                break;
+
+
+        }
+    }
 }
